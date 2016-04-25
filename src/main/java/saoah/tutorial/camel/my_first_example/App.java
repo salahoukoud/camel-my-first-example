e004ac21be59c52c934df51c93be898c41410ee5
Salah - A608978 <salahoukoud@gmail.com>
2016-04-25 11:46:36 +0100

    # Init project

---
+++ src/main/java/saoah/tutorial/camel/my_first_example/App.java
@@ -0,0 +1,13 @@
+package saoah.tutorial.camel.my_first_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
